1d8376ad9fac2a2adb27415c29f216fcd6a6bf79
Bassem Qoulta <deda9@users.noreply.github.com>
2019-04-29 21:07:27 +0200

    Update Require.swift

--- Require.swift
+++ Require.swift
@@ -1,4 +1,4 @@
 func require(_ condition: Bool, message: () -> String) {
-    let msg = "Debug crash: \(message()) in file: \(#file), line: \(#line)"
+    let msg = "Debug crash: \(message()) in"
     assert(condition, msg)
 }
